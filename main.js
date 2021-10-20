fcb2d0a02fdad74f64f104504b94f5a159e74f58
dharshancoder <92212498+dharshancoder@users.noreply.github.com>
2021-10-20 15:07:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var first_name = "Rnbir";
+var last_name = "Kapoor";
+var full_name = first_name.concat(last_name);
+
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log(add);
+
+var substract = a-b;
+console.log(substract);
+
+var multiply = a*b;
+console.log(multiply);
+
+var divide = a/b;
+console.log(divide);
